454bd91ce29fb3efcb17c2c6cb2bb62e2642807e
shijl0925 <18221510672@139.com>
2023-08-29 15:36:26 +0800

    Update build_image.sh

--- src/build_image.sh
+++ src/build_image.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-echo "CENTOS_VERSION: ${CENTOS_VERSION}"
-echo "PYTHON_VERSION: ${PYTHON_VERSION}"
-echo "QUICK_BUILD: ${QUICK_BUILD}"
-
 # Build the corresponding image
-docker build . \
+docker build --no-cache . \
   --file ./src/Dockerfile \
-  --tag ${DOCKERHUB_USERNAME}/centos-${CENTOS_VERSION}-python:${PYTHON_VERSION%.*} \
+  --tag ${DOCKERHUB_USERNAME}/centos-${CENTOS_VERSION}-python:${PYTHON_VERSION} \
   --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
   --build-arg CENTOS_VERSION=${CENTOS_VERSION} \
   --build-arg QUICK_BUILD="${QUICK_BUILD}"
