777d8bada86d8440699e13c5258294d35257887a
shijl0925 <18221510672@139.com>
2023-08-29 13:49:10 +0800

    Update build_image.sh

--- src/build_image.sh
+++ src/build_image.sh
@@ -3,9 +3,7 @@
 # Build the corresponding image
 docker build . \
   --file ./src/Dockerfile \
-  --tag ${DOCKERHUB_USERNAME}/centos-stream${CENTOS_VERSION}-python:${PYTHON_VERSION} \
-  --build-arg PY_VERSION=${PYTHON_VERSION} \
+  --tag ${DOCKERHUB_USERNAME}/centos-${CENTOS_VERSION}-python:${PYTHON_VERSION} \
+  --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
   --build-arg CENTOS_VERSION=${CENTOS_VERSION} \
-  --build-arg CREATION_COMMIT_SHA="${GITHUB_SHA}" \
-  --build-arg QUICK_BUILD="${QUICK_BUILD}" \
-  --build-arg OPENSSL_VERSION="${OPENSSL_VERSION}"
+  --build-arg QUICK_BUILD="${QUICK_BUILD}"
