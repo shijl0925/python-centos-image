591abe1fe3a891402432acfa03bf516d063bed9f
shijl0925 <18221510672@139.com>
2023-08-30 15:05:37 +0800

    Update test_image.py

--- src/3.6/test_image.py
+++ src/3.6/test_image.py
@@ -6,7 +6,6 @@ import subprocess
 if __name__ == '__main__':
     openssl = subprocess.run(
         ["/usr/bin/openssl", "version"],
-        capture_output=True,
-        text=True
-    ).stdout
-    assert ssl.OPENSSL_VERSION in openssl
+        stdout=subprocess.PIPE
+    )
+    assert ssl.OPENSSL_VERSION in str(openssl.stdout)
