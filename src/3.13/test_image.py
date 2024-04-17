895bbe02127703d3920b2e4e150b001bda405e70
Edmond Dantès <18221510672@139.com>
2024-04-17 10:50:02 +0800

    update test openssl

--- src/3.13/test_image.py
+++ src/3.13/test_image.py
@@ -1,14 +1,12 @@
 import os
 import ssl
-import sys
 
 import subprocess
 
 if __name__ == '__main__':
-    openssl_version = sys.argv[1]
 
     openssl = subprocess.run(
-        [f"/usr/local/openssl-{openssl_version}/bin/openssl", "version"],
+        [f"/usr/local/openssl/bin/openssl", "version"],
         capture_output=True,
         text=True
     ).stdout
--- src/test_image.sh
+++ src/test_image.sh
@@ -2,5 +2,5 @@
 
 docker create -t -i --name test-container ${DOCKERHUB_USERNAME}/centos-${CENTOS_VERSION}-python:${PYTHON_VERSION}
 docker start test-container
-docker exec -i test-container python3 < src/${PYTHON_VERSION%.*}/test_image.py ${OPENSSL_VERSION}
+docker exec -i test-container python3 < src/${PYTHON_VERSION%.*}/test_image.py
 # docker exec -i test-container python3 -m test -v
